b79e005a3345e3c6e1c03a4cbbad08bd44f32f36
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-02-22 11:09:55 +0700

    add: basic animation and event handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,26 @@ import { motion } from 'framer-motion';
 import './App.css';
 
 function App() {
-  return <div></div>;
+  return (
+    <div className='w-full h-screen flex justify-center items-center bg-red-200'>
+      <motion.div
+        drag
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 0.5 }}
+        whileHover={{ scale: 1.5 }}
+        whileDrag={{
+          rotate: '180deg',
+          borderRadius: '100px',
+        }}
+        whileTap={{
+          scale: 1,
+        }}
+        className='bg-red-500 text-white w-36 h-36 flex cursor-pointer justify-center items-center font-semibold rounded-md'>
+        Red Box
+      </motion.div>
+    </div>
+  );
 }
 
 export default App;
