279a35fb95c929820aa40582c38607dfed57bd81
Lionel Ritchie <lionelrtchieee@gmail.com>
2022-02-23 09:33:56 +0700

    add: text state

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,10 @@
 import { motion } from 'framer-motion';
+import { useState } from 'react';
 
 export default function IndexPage() {
+  const texts = { hover: 'Hover me', tap: 'Tap me', drag: 'Drag me' };
+  const [text, setText] = useState(texts.hover);
+
   return (
     <motion.div
       drag
@@ -8,6 +12,10 @@ export default function IndexPage() {
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
       whileHover={{ scale: 1.5 }}
+      onHoverStart={() => setText(texts.tap)}
+      onHoverEnd={() => setText(texts.hover)}
+      onTapStart={() => setText(texts.drag)}
+      onDragEnd={() => setText(texts.tap)}
       whileDrag={{
         rotate: '180deg',
         borderRadius: '100px',
@@ -16,7 +24,7 @@ export default function IndexPage() {
         scale: 1,
       }}
       className='bg-red-500 text-white w-36 h-36 flex cursor-pointer justify-center items-center font-semibold rounded-md'>
-      Red Box
+      {text}
     </motion.div>
   );
 }
